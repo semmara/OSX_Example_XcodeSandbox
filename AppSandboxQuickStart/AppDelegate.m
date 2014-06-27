fdb962a4357085a7f65b489e90e2befe3edc0ebb
semmara <semmara@users.noreply.github.com>
2014-06-27 15:54:35 +0200

    first drop handling

--- AppSandboxQuickStart/AppDelegate.m
+++ AppSandboxQuickStart/AppDelegate.m
@@ -10,9 +10,39 @@
 
 @implementation AppDelegate
 
++ (void)readFolderContent:(NSString *)path
+{
+	NSError * error;
+	NSArray * dirCont = [[NSFileManager defaultManager] contentsOfDirectoryAtPath:path error:&error];
+	NSLog(@"dir: %@", path);
+	NSLog(@"content: %@", dirCont);
+	NSLog(@" error => %@ ", [error userInfo]);
+	NSLog(@"================================================================");
+}
+
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification
 {
 	// Insert code here to initialize your application
+	
+	NSString * resourcePath = [[NSBundle mainBundle] resourcePath];
+	NSLog(@"resourcePath: %@", resourcePath);
+	[AppDelegate readFolderContent:resourcePath];
+	[AppDelegate readFolderContent:@"/tmp"];
+	[AppDelegate readFolderContent:NSHomeDirectory()];
+	[AppDelegate readFolderContent:[NSHomeDirectory() stringByAppendingString:@"/Downloads"]];
+	[AppDelegate readFolderContent:[NSHomeDirectory() stringByAppendingString:@"/Desktop"]];
+}
+
+- (BOOL)application:(NSApplication *)sender openFile:(NSString *)filename {
+	NSLog(@"openFile");
+	[AppDelegate readFolderContent:filename];
+	return YES;
+}
+
+- (void)application:(NSApplication *)sender openFiles:(NSArray *)filenames {
+	NSLog(@"openFiles");
+	[AppDelegate readFolderContent:[filenames objectAtIndex:0]];
+	//return YES;
 }
 
 - (void)awakeFromNib {
